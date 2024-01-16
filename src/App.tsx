69623fbe9d03270b4dc21f9eee57c63598fcdc5c
cryptoroshan <royalguy1031@gmail.com>
2024-01-16 15:46:24 +0000

    portfolio

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import { useEffect } from "react";
+import { config } from "./constants/config";
+
+const App = () => {
+  useEffect(() => {
+    if (document.title !== config.html.title) {
+      document.title = config.html.title;
+    }
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="bg-primary relative z-0">
+        <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <div className="relative z-0">
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
